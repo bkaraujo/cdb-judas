43597a84e9f2849fa3331ad9d1649b60562ce8dd
Bruno da Silva Perez Araújo <bkraujo@outlook.com>
2026-05-28 09:55:37 -0300

    Adiciona integração com OpenAPI (Swagger) para documentação da API
    - Adiciona a dependência do springdoc-openapi-starter-webmvc-ui no pom.xml
    - Define o caminho customizado da interface do Swagger UI para /swagger
    - Cria a classe OpenApiConfig para configurar os metadados da API e o esquema de autenticação com token via cabeçalho HTTP

---
+++ src/main/java/br/community/core/web/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package br.community.core.web;
+
+import br.community.core.web.security.LoginResource;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.val;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@NullMarked
+@Configuration
+class OpenApiConfig {
+
+    private static final String TOKEN_SCHEME = "AccessToken";
+
+    @Bean
+    public OpenAPI cdbJudasOpenAPI(ObjectProvider<BuildProperties> buildProperties) {
+        val build = buildProperties.getIfAvailable();
+        val version = build != null ? build.getVersion() : "dev";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CDB Judas API")
+                        .description("API de controle financeiro pessoal")
+                        .version(version))
+                .components(new Components()
+                        .addSecuritySchemes(TOKEN_SCHEME, new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name(LoginResource.TOKEN_HEADER)))
+                .addSecurityItem(new SecurityRequirement().addList(TOKEN_SCHEME));
+    }
+}
